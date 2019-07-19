b418a3585209ffa2f2e38ca403eafa674513a414
Zhiyuan Hu <zhu2@cappuccino.vision.cs.cmu.edu>
2019-07-19 14:31:37 -0400

    cos, factor=8

--- save_features.py
+++ save_features.py
@@ -19,7 +19,7 @@ def save_feature(args):
     network_ = LearningModule(feature_extractor_, crit=[], cls=fc_classifier_, output='feat')
     network_ = UserScatteredDataParallel(network_)
     patch_replication_callback(network_)
-    network_.load_state_dict(torch.load('ckpt_lr0.1cosdrop/net_epoch_10.pth'))
+    network_.load_state_dict(torch.load('ckpt_lr0.1cosdrop_3/net_epoch_24.pth'))
     torch.save(network_.module.state_dict(), 'tmp.pth')
 
     print('Real Loading Start')
