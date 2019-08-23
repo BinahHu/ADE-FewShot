1bb026b711e026294804f0c3eab2372eeba6d97d
Ziqi Pang <pangziqi@pku.edu.cn>
2019-08-23 14:49:19 -0400

    fgbg

--- model/component/fgbg.py
+++ model/component/fgbg.py
@@ -6,9 +6,9 @@ import json
 import cv2
 
 
-class BinaryMaskPredictor(nn.Module):
+class FGBGMaskPredictor(nn.Module):
     def __init__(self, args):
-        super(BinaryMaskPredictor, self).__init__()
+        super(FGBGMaskPredictor, self).__init__()
         self.in_dim = args.feat_dim
         self.args = args
         self.down_sampling_rate = args.down_sampling_rate
@@ -36,62 +36,7 @@ class BinaryMaskPredictor(nn.Module):
         return mask[:, int(label.item()), :, :]
 
     def diagnosis(self, agg_input):
-        """
-        visualize
-        :param agg_input:
-        :return:
-        """
-        def transform_mask_to_rgb(mask):
-            mask = np.array(mask.cpu())
-            R = ((mask / 256) * 10).astype(np.int8)
-            G = (mask - 256 * (R/10)).astype(np.int)
-            B = 125 * np.ones((mask.shape[0], mask.shape[1])).astype(np.int)
-            mask_img = np.zeros((mask.shape[0], mask.shape[1], 3))
-            mask_img[:, :, 0] = B
-            mask_img[:, :, 1] = G
-            mask_img[:, :, 2] = R
-            mask_img = mask_img.astype(np.int)
-            return mask_img
-
-        feature_map = agg_input['feature_map']
-        anchors = agg_input['anchors'].int()
-        scale = agg_input['scales']
-        mask = agg_input['seg']
-        labels = agg_input['labels']
-        anchor_num = anchors.shape[0]
-        mask_img = transform_mask_to_rgb(mask)
-        cv2.imwrite('diagnosis/data/mask/{}.jpg'.format(self.diagnosis_count), mask_img)
-
-        feature_map = feature_map.unsqueeze(0)
-        predicted_map = self.fc1(feature_map)
-        predicted_map = F.interpolate(predicted_map, size=(mask.shape[0], mask.shape[1]))
-        mask = mask.unsqueeze(0)
-        anchors = self.compute_anchor_location(anchors, scale, [1, 1])
-
-        # enumerate the anchors and compute the loss
-        for i in range(anchor_num):
-            anchor = anchors[i]
-            label = labels[i]
-
-            selected_map = predicted_map[:, :, anchor[2]:anchor[3], anchor[0]:anchor[1]]
-            pred_mask = F.sigmoid(5 * self.binary_transform(selected_map, label))
-
-            tgt_mask = mask[:, anchor[2]:anchor[3], anchor[0]:anchor[1]]
-            # convert into 0-1 mask
-            ones = torch.ones(tgt_mask.shape[0], tgt_mask.shape[1], tgt_mask.shape[2]).cuda()
-            zeros = torch.zeros(tgt_mask.shape[0], tgt_mask.shape[1], tgt_mask.shape[2]).cuda()
-            tgt_mask = torch.where(tgt_mask == self.base_classes[int(label.item())], ones, zeros)
-            tgt_mask = np.array(255 * tgt_mask[0].cpu()).astype(np.int)
-            cv2.imwrite('diagnosis/data/mask/{}_tgt_{}.jpg'.format(self.diagnosis_count, i), tgt_mask)
-            # pred_mask = torch.where(pred_mask >= 0, ones, zeros)
-            pred_mask = np.array(255 * pred_mask[0].detach().cpu()).astype(np.int)
-            cv2.imwrite('diagnosis/data/mask/{}_pre_{}.jpg'.format(self.diagnosis_count, i), pred_mask)
-
-        self.diagnosis_count += 1
-        if self.diagnosis_count == 80:
-            exit(0)
-        return torch.tensor([0]).cuda()
-
+        raise NotImplementedError
 
     def forward(self, agg_input):
         """
