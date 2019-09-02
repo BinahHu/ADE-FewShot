d58f6a1462960179bd8c5b1de087c783646e9737
胡致远 <2016011260@secoder.net>
2019-09-02 09:37:38 -0400

    stage

--- preprocessing/supervison_generation/seg.py
+++ preprocessing/supervison_generation/seg.py
@@ -36,7 +36,7 @@ def generate_seg(args):
             raise RuntimeError('{} not exists'.format(seg_path_original))
         seg_path = img_path[:-4] + '_seg_base.png'
         seg_paths.append(seg_path)
-
+        '''
         segmentation = cv2.imread(os.path.join('../../../' + seg_path_original))
         B, G, R = np.transpose(segmentation, (2, 0, 1))
         seg_map = (G + 256 * (R / 10))
@@ -48,7 +48,7 @@ def generate_seg(args):
                 else:
                     seg_map[h, w] = base_map[seg_map[h, w]]
         cv2.imwrite('../../../' + seg_path, seg_map.astype(np.uint8))
-
+        '''
         if i % 1 == 0:
             print('{} / {}'.format(i, length))
 
