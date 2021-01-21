96df201cf21dcb4f716bb00a90a598dada7fe0d0
胡致远 <2016011260@secoder.net>
2021-01-21 21:49:17 +0800

    stage

--- train2.sh
+++ train2.sh
@@ -1,4 +1,5 @@
 srun -p long -t 48:00:00 -w espresso-0-24 --gres=gpu:4 --pty \
-python train.py --comment pure_rotation_pretrain --ckpt ckpt/pure_rotation_pretrain --start_epoch 0 --num_epoch 10 \
---list_train ./data/ADE/ADE_Base/base_img_train.json --lr_feat 1e-1 --lr_cls 0 \
---model_weight ''
+python train.py --comment rotation_pretrain_baseline --ckpt ckpt/rotation_pretrain_baseline --start_epoch 1 --num_epoch 8 \
+--list_train ./data/ADE/ADE_Base/base_img_train.json --lr_feat 1e-1 --lr_cls 1e-1 \
+--supervision null.json \
+--model_weight ckpt/pure_rotation_pretrain/net_epoch_9.pth
